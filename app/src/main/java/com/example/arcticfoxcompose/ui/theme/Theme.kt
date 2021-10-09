d9ef839188375674d9a4c7e4920cc3c922985779
Nosov Roman <roma2000rus@gmail.com>
2021-10-09 19:10:29 +0300

    review colors first screen

--- app/src/main/java/com/example/arcticfoxcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/arcticfoxcompose/ui/theme/Theme.kt
@@ -10,7 +10,9 @@ import androidx.compose.ui.graphics.Color
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    onSurface = DarkText,
+    onSecondary = HintColor
 )
 
 private val LightColorPalette = lightColors(
